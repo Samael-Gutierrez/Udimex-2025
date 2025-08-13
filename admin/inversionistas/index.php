2d83875624ae83dc8dfb50009388d929a951cd73
Samael Gutierrez <al221911279@gmail.com>
2025-08-13 12:13:16 -0600

    Notmalizacion de inversionistas

--- admin/inversionistas/index.php
+++ admin/inversionistas/index.php
@@ -1,104 +1,59 @@
-<!DOCTYPE HTML>
 <?php
 session_start();
-include("../funciones.php");
-include("../../general/consultas/basic.php");
-include("../../general/consultas/admin.php");
-include("../../general/consultas/inv.php");
-
 date_default_timezone_set('America/Mexico_City');
 
+$dir = "../../general/";
+include($dir . "db/inv.php");
+include($dir . "db/basica.php");
+include($dir . "db/admin.php");
+include($dir . "php/admin.php");
 
-//permiso();
-cabeza("Inversionistas - Udimex","");
+cabeza("Inversionistas - Udimex", "", "");
+echo "<body>";
+usuario("../../", 1);
+echo "<center>";
+menu_i();
+echo "<br><fieldset id='subtitulo'>CONTROL DE PAGOS</fieldset>";
 
+$fact = date('Y-m-d');
 
-
-
-
-
-
-?>
-
-
-
-<body>
-
-<?php
-
-
-
-
-
-	usuario("../../",1);
-	echo "<center>";
-	menu_i();
-	echo "<br><br><br><br>
-		<fieldset id='subtitulo'>CONTROL DE PAGOS</fieldset>";
-
-	
-
-
-	$fact=date('Y-m-d');
-
-	/*Buscar todos los grupos*/
-	
-
-	$grupo="";
-	$cg="<tr><th>Invesionista</th><th>Capital</th><th>Nuevo Aporte</th></tr>";
-	$sg="";
-	$datos=b_inv2();
-	while($fila=mysqli_fetch_assoc($datos)){
-		$datos2=b_depo2($fila['id_usuario']);
-		if($fila2=mysqli_fetch_assoc($datos2)){
-			$inversion=$fila2['r'];
-		}
-		else{
-			$inversion=0;
-		}
-
-
-
-			
-
-
-
-
-
-
-
-				$fpag="<form method='POST' action='pago_guarda.php'>
-					<input type='text' value='' name='cantidad' size='5'>
-					<input type='hidden' value='".$fila['id_usuario']."' name='usuario'>
-					<input type='submit' value='Guardar'>
-				</form>";
-
-				
-
-
-			
-			
-
-
-			$cg=$cg."<tr>
-				<td>".$fila['id_usuario']."</td>
-				<td>".$fila['nombre']." ".$fila['ap_pat']." ".$fila['ap_mat']."</td>
-				<td align='center'>$inversion</td>
-				<td align='center'>$fpag</td>
-			</tr>";
-	
-
-
-
-
-		
+/*Buscar todos los grupos*/
+$grupo = "";
+$cg = "
+	<tr>
+		<th>Invesionista</th>
+		<th>Capital</th>
+		<th>Nuevo Aporte</th>
+	</tr>
+";
+$sg = "";
+$datos = b_inv2();
+while ($fila = mysqli_fetch_assoc($datos)) {
+	$datos2 = b_depo2($fila['id_usuario']);
+	if ($fila2 = mysqli_fetch_assoc($datos2)) {
+		$inversion = $fila2['r'];
+	} else {
+		$inversion = 0;
 	}
-	
-	echo "<table border='1'>".$cg."</table>";
 
+	$fpag = "
+		<form method='POST' action='pago_guarda.php'>
+			<input type='text' value='' name='cantidad' size='5'>
+			<input type='hidden' value='" . $fila['id_usuario'] . "' name='usuario'>
+			<input type='submit' value='Guardar'>
+		</form>";
 
+	$cg = $cg . "
+		<tr>
+			<td>" . $fila['id_usuario'] . "</td>
+			<td>" . $fila['nombre'] . " " . $fila['ap_pat'] . " " . $fila['ap_mat'] . "</td>
+			<td align='center'>$inversion</td>
+			<td align='center'>$fpag</td>
+		</tr>
+	";
+}
+
+echo "<table border='1'>$cg</table>";
 ?>
-
-
 </body>
 </html>
--- general/db/inv.php
+++ general/db/inv.php
@@ -1,34 +1,33 @@
 <?php
 function b_inv($id){
-	$consulta="SELECT count(id_inversionista) as r FROM inversionista where id_usuario=$id and estado>0";
-	return completa($consulta);
+	$consulta="SELECT count(id_inversionista) AS r FROM inversionista WHERE id_usuario = ? AND estado>0";
+	return ejecuta($consulta, [$id], 0);
 }
 
 function b_inv2(){
-	$consulta="SELECT * FROM inversionista as i, usuario as u where i.id_usuario=u.id_usuario and i.estado>0 and u.estado>0 order by u.ap_pat, i.id_inversionista";
-	return completa($consulta);
+	$consulta="SELECT * FROM inversionista as i, usuario as u WHERE i.id_usuario=u.id_usuario AND i.estado>0 AND u.estado>0 ORDER BY u.ap_pat, i.id_inversionista";
+	return ejecuta($consulta, [], 0);
 }
 
-
 function b_depo($id){
-	$consulta="SELECT * FROM inv_depo where id_usuario=$id and estado>0 order by fi";
-	return completa($consulta);
+	$consulta="SELECT * FROM inv_depo WHERE id_usuario = ? AND estado>0 ORDER BY fi";
+	return ejecuta($consulta, [$id], 0);
 }
 
 function b_depo2($id){
-	$consulta="SELECT sum(cantidad) as r FROM inv_depo where id_usuario=$id and estado>0";
-	return completa($consulta);
+	$consulta="SELECT sum(cantidad) as r FROM inv_depo WHERE id_usuario = ? AND estado>0";
+	return ejecuta($consulta, [$id], 0);
 }
 
 function b_reserva($val){
-	$consulta="SELECT * FROM inv_aux where nombre like '$val'";
-	return completa($consulta);
+	$consulta="SELECT * FROM inv_aux WHERE nombre LIKE ? ;";
+	return ejecuta($consulta, [$val], 0);
 }
 
 function g_inv($cantidad,$us){
 	$fecha=date('Y-m-d');
-	$consulta="INSERT INTO inv_depo (fi, cantidad, id_usuario, estado) VALUES ('$fecha', $cantidad, $us, 1);";
-	completa($consulta);
+	$consulta="INSERT INTO inv_depo (fi, cantidad, id_usuario, estado) VALUES(?, ?, ?, ?);";
+	ejecuta($consulta, [$fecha, $cantidad, $us, 1], 0);
 }
 
 ?>
